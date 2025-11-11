57a07b9b1b2444f5f164d3e1e1d1257912bdee5e
jhon <sanchezmunozjhon@gmail.com>
2025-11-11 16:05:13 -0500

    prueba para error de pipeline1

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,3 @@
-// eslint.config.js
 import tseslint from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
 import eslintPluginPlaywright from 'eslint-plugin-playwright';
@@ -8,6 +7,10 @@ export default [
   {
     ignores: ['ci-report/**', 'test-results/**', 'reports/**'],
   },
+
+  // -------------------------
+  // ✅ Reglas para TODO el repo
+  // -------------------------
   {
     files: ['**/*.ts'],
     languageOptions: {
@@ -24,9 +27,45 @@ export default [
     rules: {
       ...tseslint.configs.recommended.rules,
       ...eslintPluginPlaywright.configs['flat/recommended'].rules,
-      'playwright/no-conditional-in-test': 'off',
-      'playwright/no-wait-for-timeout': 'off',
+
+      // Estándares recomendados
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/consistent-type-imports': 'error',
+
+      // CamelCase para todo el repo
+      '@typescript-eslint/naming-convention': [
+        'error',
+        {
+          selector: 'variable',
+          format: ['camelCase', 'UPPER_CASE'],
+          leadingUnderscore: 'allow',
+        },
+      ],
     },
   },
+
+  // ------------------------------------------------------
+  // ✅ Overrides: Reglas ESPECÍFICAS para pruebas Playwright
+  // ------------------------------------------------------
+  {
+    files: ['tests/**/*.ts', '**/*.spec.ts'],
+    rules: {
+      // Permitimos expect condicionales en WebKit, etc.
+      'playwright/no-conditional-in-test': 'off',
+
+      // A veces obligan timeouts controlados
+      'playwright/no-wait-for-timeout': 'off',
+
+      // No exigir expect obligatorio en tests "smoke"
+      'playwright/expect-expect': 'warn',
+
+      // Permitir nombres en minúscula para fixtures
+      '@typescript-eslint/naming-convention': 'off',
+    },
+  },
+
+  // -------------------------
+  // ✅ Prettier al final SIEMPRE
+  // -------------------------
   prettierConfig,
 ];
--- src/pages/LoginPage.ts
+++ src/pages/LoginPage.ts
@@ -68,6 +68,7 @@ export class LoginPage extends BasePage {
   }
 
   async assertErrorEquals(expected: string) {
+    const bad_variable_name = 123;
     const msg = await this.getError();
     expect(msg).toBe(expected);
   }
